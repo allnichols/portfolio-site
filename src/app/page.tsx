5b4c2a3b1613f3e99b20642cfeda01ddfe99a7e1
Max <allenmaxnichols@gmail.com>
2025-05-05 16:11:31 -0500

    remove commented out code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,26 +44,6 @@ export default function Home() {
             </button>
           </div>
         </section>
-        {/* <section id="about">
-          <p className={`${oxygen.className} font-medium text-[16px]`}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            tristique, urna at tincidunt vehicula, justo erat tincidunt eros,
-            nec tincidunt sapien justo non nisi. Integer vel libero nec nulla
-            facilisis tincidunt.
-          </p>
-          <p className={`${oxygen.className} font-medium text-[16px] `}>
-            Curabitur eget velit nec nulla fermentum tincidunt. Proin
-            scelerisque, sapien nec tincidunt tincidunt, lorem ipsum tincidunt
-            sapien, nec tincidunt sapien lorem nec sapien. Donec tincidunt
-            tincidunt sapien nec tincidunt.
-          </p>
-          <p className={`${oxygen.className} font-medium text-[16px]`}>
-            Fusce tincidunt sapien nec tincidunt tincidunt. Sed tincidunt sapien
-            nec tincidunt tincidunt. Integer tincidunt sapien nec tincidunt
-            tincidunt. Vestibulum tincidunt sapien nec tincidunt tincidunt
-            tincidunt tincidunt sapien.
-          </p>
-        </section> */}
       </main>
       {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer> */}
     </Container>
