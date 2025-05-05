a5fa319d04188bf7775882822a4e3bcfbbdd3a9a
Max <allenmaxnichols@gmail.com>
2025-05-05 14:52:07 -0500

    add pages and fonts

---
+++ src/app/ui/NavBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Links = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About" },
+  { href: "/projects", label: "Projects" },
+  { href: "/experience", label: "Experience" },
+  { href: "/contact", label: "Contact" },
+];
+
+export default function NavBar() {
+  return (
+    <nav className="fixed bottom-0 left-0 w-full gb-white bordet-t md:static md:border-none md:bg-transparent">
+      <ul className="flex justify-around md:justify-start md:gap-8 p-4 md:p-0">
+        {Links.map((link) => (
+          <li key={link.label}>
+            <Link
+              href={link.href}
+              className="text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Poppins, Signika } from "next/font/google";
+
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
+export const signika = Signika({ subsets: ["latin"] });
