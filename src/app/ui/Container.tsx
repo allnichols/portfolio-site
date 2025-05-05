0c495bca2704f26583b9eff85d5a0fc8d834f097
Max <allenmaxnichols@gmail.com>
2025-05-05 15:54:00 -0500

    add container and logo

--- src/app/ui/Container.tsx
+++ src/app/ui/Container.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/app/ui/NavBar";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-[24px]">
+    <div className="container m-auto p-[24px]">
       <NavBar />
       {children}
     </div>
