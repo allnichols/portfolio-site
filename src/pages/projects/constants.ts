f6b9022b9dfb21370fa7c30b52d05a37fb76d25f
Max <allenmaxnichols@gmail.com>
2025-05-22 16:17:40 -0500

    add projects

---
+++ src/pages/projects/constants.ts
@@ -0,0 +1,23 @@
+export const Projects = [
+  {
+    name: "School Management System",
+    image: "https://picsum.photos/id/8/200/150",
+    description:
+      "A comprehensive platform designed to help small schools manage students, classes, attendance, and grades efficiently.",
+    tech: [
+      "React",
+      "Typescript",
+      "Tailwind CSS",
+      "Shadcn",
+      "Spring Boot",
+      "PostgreSQL",
+    ],
+  },
+  {
+    name: "SpaceGrid",
+    image: "https://picsum.photos/id/84/200/150",
+    description:
+      "A web application for space enthusiasts to explore real-time satellite data, and track orbits.",
+    tech: ["React", "Typescript", "Tailwind CSS", "Shadcn", "Next.js"],
+  },
+];
